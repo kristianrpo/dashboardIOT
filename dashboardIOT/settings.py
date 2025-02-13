2137ff64abe366900f6d30ce719daac232167f5c
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-02-13 10:56:12 -0500

    fix: correct static files

--- dashboardIOT/settings.py
+++ dashboardIOT/settings.py
@@ -89,3 +89,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "dashboardIOT/static/",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
