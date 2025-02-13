b2635d22972be82515717a179896a750a07d1689
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-02-13 11:25:05 -0500

    fix: database problems

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+import os
+import shutil
 from dashboardIOT.wsgi import application
 
-app = application 
+db_path = "/tmp/db.sqlite3"
+if not os.path.exists(db_path):
+    shutil.copyfile("db.sqlite3", db_path)
+
+app = application
