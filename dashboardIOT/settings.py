e6b5ffb5050857d0afe26fbefffac2af6ebab657
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-02-13 11:04:53 -0500

    fix: add static files whitenoise line

--- dashboardIOT/settings.py
+++ dashboardIOT/settings.py
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'dashboardIOT.urls'
 
 TEMPLATES = [
